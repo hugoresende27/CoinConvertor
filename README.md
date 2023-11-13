ae2c01054f947be48555b9331fbeb770bc2fb819
hugoresende <hugo.resende@iservices.pt>
2023-11-13 21:17:18 +0000

    aula 1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ### Aula 1 ::: https://www.youtube.com/watch?v=F8NoweGVyD8&ab_channel=Alura
+
+#### Docs JS : https://developer.mozilla.org/en-US/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-alert('hello world')
+// alert('hello world')
+var euroValue = 100;
+var dolarCot = 5.25;
+var finalValue = euroValue * dolarCot;
+
+var meters = 1000;
+var lightYears = meters / 9.461e+15;
+alert (euroValue + ' € são ' + finalValue.toFixed(2) + ' reais');
+alert (meters + ' metros são ' + lightYears + ' anos luz');
