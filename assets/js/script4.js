d4fe7f7393aa5bfea6ac77b5bdb4852a92ec36ee
hugoresende <hugo.resende@iservices.pt>
2023-11-18 10:25:05 +0000

    aula 4

--- assets/js/script4.js
+++ assets/js/script4.js
@@ -1,3 +1,14 @@
 function adicionarFilme() {
-    console.log('addmovie')
+    console.log('addmovie');
+
+    var favMovie = document.getElementById('movie').value;
+    var movieList = document.getElementById('movieList');
+
+    // alert(favMovie);
+    // document.write("<img src=" + favMovie + ">");
+    movieList.innerHTML = movieList.innerHTML + "<img src=" + favMovie + ">";
+
+    document.getElementById('movie').value = "";
+  
+
 }
