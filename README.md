49a05e3f7536302311980bd5dde0615ecd068f6d
hugoresende <hugo.resende@iservices.pt>
2023-11-18 11:07:01 +0000

    aula 5

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 ### Aula 1 ::: https://www.youtube.com/watch?v=F8NoweGVyD8&ab_channel=Alura
 ### Aula 2 ::: https://www.youtube.com/watch?v=LV_yrV3SSb8
+### Aula 3 ::: https://youtu.be/VaU_YO7DKco
+### Aula 4 ::: https://youtu.be/p7vaK8KRx34
+### Aula 5 ::: https://youtu.be/bG2cFAT2Qas
 
 #### Docs JS : https://developer.mozilla.org/en-US/
---
+++ assets/js/script5.js
@@ -0,0 +1,62 @@
+var paulo = {
+    name:'Paul',
+    vitories : 0,
+    draws: 0,
+    loss: 0,
+    points: 0
+};
+
+var rafa = {
+    name:'Rafa',
+    vitories : 0,
+    draws: 0,
+    loss: 0,
+    points: 0
+};
+
+// alert (first.vitories);
+
+var table = document.getElementById('playersTable');
+
+showTable();
+
+function showTable () {
+    table.innerHTML =  `
+    <tr>
+        <td>`+ paulo.name +`</td>
+        <td>${paulo.vitories}</td>
+        <td>${paulo.draws}</td>
+        <td>${paulo.loss}</td>
+        <td>${paulo.points}</td>
+        <td><button onClick="addVictory(paulo)">Vitória</button></td>
+        <td><button onClick="addDraw(paulo)">Empate</button></td>
+        <td><button onClick="addLoss(paulo)">Derrota</button></td>
+    </tr>
+
+`
+
+
+};
+
+
+
+function addVictory(player) {
+    player.vitories ++;
+    player.points += 3;
+    console.log(player);
+    showTable();
+
+}
+function addDraw(player) {
+    player.draws ++;
+    player.points += 1;
+    console.log(player);
+    showTable();
+
+}
+function addLoss(player) {
+    player.loss ++;
+    console.log(player);
+    showTable();
+    
+}
